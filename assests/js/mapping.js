be09b59d2d132b3206f7f69e726d994cfc08e846
Gunner Dumas <176344388+gunnerdumas@users.noreply.github.com>
2026-07-07 13:17:35 -0400

    working overlay

--- assests/js/mapping.js
+++ assests/js/mapping.js
@@ -1,20 +1,42 @@
 var map = L.map('map').fitWorld();
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// maps
+var osmSkiTrails = L.tileLayer('https://tile.waymarkedtrails.org/slopes/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data © OpenStreetMap contributors, OpenSnowMap'
+}).addTo(map);
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-map.locate({setView: true, maxZoom: 16})
+osm.addTo(map);
+osmSkiTrails.addTo(map);
 
-function onLocationFound(e) {
-    var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map).bindPopup("Your within " + radius + "meters from here").openPopup();
-    L.circle(e.latlng, radius).addTo(map);
+// map controls and setup
+var baseMaps = {
+    "OpenStreetMap": osm,
+    }
+
+var overlayMaps = {
+    "SkiTrails": osmSkiTrails
 }
-function onLocationError(e) {
-    alert(e.message);
-}
+    
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
+
+// gps setup and activate
+// map.locate({setView: true, maxZoom: 16})
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
+
+//     L.marker(e.latlng).addTo(map).bindPopup("Your within " + radius + " meters from here").openPopup();
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+// function onLocationError(e) {
+//     alert(e.message);
+// }
+
+// map.on('locationfound', onLocationFound);
+// map.on('locationerror', onLocationError);
