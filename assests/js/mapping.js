70a70705008aa8b978202ab94c23c55e8c0c6c75
Gunner Dumas <176344388+gunnerdumas@users.noreply.github.com>
2026-07-07 12:18:32 -0400

    get location from phone

--- assests/js/mapping.js
+++ assests/js/mapping.js
@@ -1,5 +1,20 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').fitWorld();
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+map.locate({setView: true, maxZoom: 16})
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map).bindPopup("Your within " + radius + "meters from here").openPopup();
+    L.circle(e.latlng, radius).addTo(map);
+}
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
